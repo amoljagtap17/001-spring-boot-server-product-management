71e826d87be1159dd30d66e58fbf91c3e672bed3
Amol Jagtap <amoljagtap17@gmail.com>
2019-06-28 20:36:59 +0530

    SPRING-BOOT-SERVER-PRODUCT-MANAGEMENT: Initial Spring Boot Repo

---
+++ src/main/java/com/react/springboot/serverproductmanagement/ServerProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.react.springboot.serverproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerProductManagementApplication.class, args);
+    }
+
+}
